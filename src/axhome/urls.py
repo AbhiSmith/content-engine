054a157307be22d05bc64f53f0640be8c999d891
AbhiSmith <abhishekv4004@gmail.com>
2024-03-07 00:02:20 +0530

    create django app

--- src/axhome/urls.py
+++ src/axhome/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for cfehome project.
+URL configuration for axhome project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
-
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.home_page),
+    path('admin/', admin.site.urls),
 ]
